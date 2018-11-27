c4ba56ed4153237b1313030b780891b99dfb4532
Andreas Groos <andreas.groos1@gmail.com>
2018-11-27 09:15:26 -0800

    Minimal <TRow> and <TCol> components

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import MyComponent from "../../src";
-const App = () => <MyComponent />;
+import { RedHeadline, TRow, TCol } from "../../src";
+const App = () => (
+  <div style={{ height: "500px" }}>
+    <TRow fillHeight justifyContent="end">
+      <TCol centered>
+        <h1>Hello</h1>
+      </TCol>
+    </TRow>
+  </div>
+);
 render(<App />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const RedHeadline = styled.h1`
-  color: red;
+const TRow = styled.div`
+  display: flex;
+  justify-content: ${props =>
+    props.justifyContent === "center"
+      ? "center"
+      : props.justifyContent === "start" ||
+        props.justifyContent === "flex-start"
+      ? "flex-start"
+      : props.justifyContent === "end" || props.justifyContent === "flex-end"
+      ? "flex-end"
+      : props.justifyContent === "between" ||
+        props.justifyContent === "space-between"
+      ? "space-between"
+      : props.justifyContent === "around" ||
+        props.justifyContent === "space-around"
+      ? "space-around"
+      : props.justifyContent === "evenly" ||
+        props.justifyContent === "space-evenly"
+      ? "space-evenly"
+      : null};
+  align-items: ${props =>
+    props.alignItems === "start" || props.alignItems === "flex-start"
+      ? "flex-start"
+      : props.alignItems === "end" || props.alignItems === "flex-end"
+      ? "flex-end"
+      : props.alignItems === "center"
+      ? "center"
+      : props.alignItems === "stretch"
+      ? "stretch"
+      : props.alignItems === "baseline" || props.alignItems === "base"
+      ? "baseline"
+      : null};
+  flex-wrap: ${props => props.wrap};
+  border: 1px red solid;
+  min-height: ${props => (props.fillHeight ? "100%" : null)};
+  & > div {
+    display: ${props => (props.centered ? "flex" : null)};
+    justify-content: ${props => (props.centered ? "center" : null)};
+    align-items: ${props => (props.centered ? "center" : null)};
+    /* flex: ${props => (props.centered ? 1 : null)}; */
+  }
 `;
 
-const MyComponent = () => <RedHeadline>Some Headline</RedHeadline>;
+const TCol = styled.div`
+  display: flex;
+  justify-content: ${props => (props.centered ? "center" : null)};
+  align-items: ${props => (props.centered ? "center" : null)};
+  flex: ${props => (props.centered ? 1 : null)};
+  border: 1px blue solid;
+`;
 
-export default MyComponent;
+export { TRow, TCol };
